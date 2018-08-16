d88865ceabf02b1fd017110afb54fcfb219b9d9b
Kristen Hunter-Cevera <kristenhunter-cevera@v-128-128-10-226.whoi.edu>
2018-08-16 13:55:02 -0400

    syringe QC

--- fcb_processing/miscellaneous/syringe_acqtime_QC2.m
+++ fcb_processing/miscellaneous/syringe_acqtime_QC2.m
@@ -1,8 +1,7 @@
-function syringe_acqtime_QC2(finv,ordered_acq,oind,ss,varargin)
-
+function syringe_acqtime_QC2(finv,ordered_acq,oind,ss,gca)
+%plot expected acquisition times following a normal distribution 
 n=length(ordered_acq);
-subplot(1,2,1,'replace')
-plot(finv,ordered_acq,'o'), hold on
+plot(gca,finv,ordered_acq,'o'), hold on
 
 if oind(end)==1 || oind(end)==n
     plot(finv(end),ordered_acq(end),'ro','markerface','r')
@@ -11,17 +10,6 @@ end
 line([ordered_acq(1) ordered_acq(end)],[ordered_acq(1) ordered_acq(end)]) %expectation
 title(['sum of squares:' num2str(ss)])
 
-subplot(1,2,2,'replace')
-
-if ~isempty(varargin)
-    finv2=varargin{1};
-    ordered_acq2=varargin{2};
-    ss2=varargin{3};
-    plot(finv2,ordered_acq2,'o')
-    line([ordered_acq2(1) ordered_acq2(end)],[ordered_acq2(1) ordered_acq2(end)])    
-    title(['sum of squares:' num2str(ss2) ' | std: ' num2str(std(ordered_acq))])
-    
-end
 end %syringe acqtime plotting function
 
 
