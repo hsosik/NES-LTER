d0cadfbb9cb63279312d0d2c3e2c3f341dd88af6
Kristen Hunter-Cevera <kristenhunter-cevera@v-128-128-10-205.whoi.edu>
2018-12-10 15:30:53 -0500

    other adjustments to handle records with gaps

--- fcb_processing/miscellaneous/fcb_load_timefile.m
+++ fcb_processing/miscellaneous/fcb_load_timefile.m
@@ -1,13 +1,17 @@
 clear all
 close all
-year=2013;
-pathname='/home/kristen/Desktop/MVCO_FCB_time_testfiles/';
-eval(['filelist=dir(''/home/kristen/Desktop/MVCO_FCB_time_testfiles/*' num2str(year) '*.mat'')'])
+year=2007;
+%pathname='/home/kristen/Desktop/MVCO_FCB_time_testfiles/';
+%eval(['filelist=dir(''/home/kristen/Desktop/MVCO_FCB_time_testfiles/*' num2str(year) '*.mat'')'])
+
+pathname='/Volumes/Lab_data/MVCO/FCB/MVCO_Mar2007/data/processed_July2016/time/';
+%eval(['filelist=dir(''' pathname '*_*.mat'')'])
+eval(['filelist=dir(''' pathname '*' num2str(year) '*.mat'')'])
 syrplotflag=1;
 %%
 
 clearvars -except year filelist pathname syrplotflag
-j=16;
+j=11;
 filename=filelist(j).name(1:end-6);
 load(strcat(pathname,filelist(j).name))
 eval(['temp=' filename ';'])
