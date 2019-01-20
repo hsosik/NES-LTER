b586d73a2a112584d16adbacd08ef33f803e0ba4
Heidi M. Sosik <hsosik@whoi.edu>
2019-01-20 13:08:12 -0500

    commit version with updated comments and sample volume included

--- Attune/not_in_use/FCS_DateTimeList.m
+++ Attune/not_in_use/FCS_DateTimeList.m
@@ -1,10 +1,8 @@
 function [ FCSfileinfo ] = FCS_DateTimeList(fcs_path)
-%function [ FCSfileinfo ] = FCS_DateTimeList(fcs_path)
+%UNTITLED2
 %input: path to a directory of fcs files
-%output: a structure called fcsfileinfo with matlab date start and stop
-%times and other fcs hdr information
-%
-%Heidi M. Sosik, Woods Hole Oceanographic Institution, Jan 2019
+%output: a structure called fcsfileinfo with matlab date starts and stops
+%   Detailed explanation goes here
 
 if ~exist('fcs_path', 'var')
     fcs_path = uigetdir(pwd, 'Pick a Directory of FCS files')
@@ -18,10 +16,8 @@ end
 
 fcslist = dir(fullfile(fcs_path, '*.fcs'));
 fcslist = {fcslist.name}';
-
 FCSfileinfo.matdate_start = NaN(size(fcslist));
 FCSfileinfo.matdate_stop = FCSfileinfo.matdate_start;
-FCSfileinfo.vol_analyzed = FCSfileinfo.matdate_start;
 for ii = 1:length(fcslist)
     if ~rem(ii,10)
         disp([num2str(ii) ' of ' num2str(length(fcslist))])
@@ -29,7 +25,6 @@ for ii = 1:length(fcslist)
     [~,fcshdr] = fca_readfcs(fullfile(fcs_path, fcslist{ii}));
     FCSfileinfo.matdate_start(ii) = datenum([fcshdr.date ', ' fcshdr.starttime]);
     FCSfileinfo.matdate_stop(ii) = datenum([fcshdr.date ', ' fcshdr.stoptime]);
-    FCSfileinfo.vol_analyzed(ii) = fcshdr.VOL;
 end
 
 FCSfileinfo.filelist = fcslist;
