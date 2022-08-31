8f1f74783633877a9e112a3276dc6a5294895d58
Bethany Fowler <beefowler@users.noreply.github.com>
2022-08-31 11:21:05 -0400

    assign_class functions for EDI

---
+++ Attune/assign_class_EN617.m
@@ -0,0 +1,194 @@
+function [ class, bounds ] = assign_class_EN617( fcsdat, fcshdr, plot_flag, filename, QC_flag, startdate )
+
+
+%different gates for different portions of the cruise
+if startdate < 7.372638900810186e+05 %7.372638680555555e+05
+    phase = 1 ;
+elseif startdate < 7.372648298611111e+05
+    phase= 2;
+elseif startdate < 7.372659375000000e+05
+    phase = 3; 
+else 
+    phase = 4; 
+end
+
+
+
+%Initialze class vector
+    class = zeros(size(fcsdat,1),1);
+    
+    %parameter numbers for main euk polygon
+    npar_eukX = 15; %BL3-H %chlorophyll 
+    npar_eukY = 19; %GL1-H %phycoerythrin 
+    
+    %parameter numbers for main syn polygon
+    npar_synX = 12; %11 is FSC-H, 12 is SSC-H
+    npar_synY = 19; %GL1-H %phycoerythrin 
+   
+    
+    %just for initial gates
+    synmaxY = 180000; 
+    synminX = 5 ; 
+    synXcorners = [300 500]; 
+
+    eukminX = 175; 
+    eukcorner = [10000 300]; 
+    eukmaxY = 2e4; 
+    eukmaxYlower = 150; 
+
+    gl2_noise_thresh = 150; %basically synminY 
+  
+
+    synGL1A2GL1Hmax = 4; %PE area to height
+    synGL1H2BL3Hslope = 1.4; %PE to CHL
+    synGL1H2BL3Hoffset = -1.0; %PE to CHL 
+    syneukBL3H2SSCHslope = 1.3; %CHL to SSC
+    syneukBL3H2SSCHoffset = -1.8; %PE to CHL 
+    nonsynfactorA = 15; %6
+    nonsynfactorB = 6; %2.5
+
+    if phase ==2
+        eukminX = 1400;
+        synGL1H2BL3Hslope = 1.3; %PE to CHL
+        gl2_noise_thresh = 450; %basically synminY 
+
+    end
+
+    if phase ==4
+        eukmaxYlower = 400; 
+        eukcorner = [10000 400];
+        gl2_noise_thresh = 400; 
+
+    end
+
+
+    %syn main gate
+    gsyn_main_gate = [synminX gl2_noise_thresh ; synXcorners(1) gl2_noise_thresh; synXcorners(2) synmaxY; synminX synmaxY]; %[Xmin Ymin; Xmax Ymax]
+    %euk gate 
+    geuk_main_gate = [eukminX eukmaxYlower;  eukcorner(1) eukcorner(2); 1100000 eukmaxY; 1100000 1; eukminX 1];
+    
+ 
+        
+    %find indices of cells within the gates
+    fcsdatlog = log10(fcsdat); %use log10 to make sure inpolygon corresponds to view of polygon on log-log plots
+    in_euk = inpolygon(fcsdatlog(:,npar_eukX),fcsdatlog(:,npar_eukY),log10(geuk_main_gate(:,1)),log10(geuk_main_gate(:,2))); 
+    in_syn = (inpolygon(fcsdatlog(:,npar_synX),fcsdatlog(:,npar_synY),log10(gsyn_main_gate(:,1)),log10(gsyn_main_gate(:,2))));
+    
+    %first look in gates, then cut out extremes? or add if pileup at edges
+    minX = prctile(fcsdat(in_syn,npar_synX),10)*.3; maxX = prctile(fcsdat(in_syn, npar_synX), 90)*10; 
+    minY = prctile(fcsdat(in_syn,npar_synY),10)* .3; 
+    maxY = prctile(fcsdat(in_syn,npar_synY),95)*10;
+
+
+    %compare density of two syn minimum threshhold options
+    if phase ~= 4; 
+    [f, xi] = ksdensity(log10(fcsdat(in_syn & log10(fcsdat(:, npar_synY))< 4, npar_synY)));
+    if sum(islocalmin(f))>0 & f(islocalmin(f))./sum(f) < .015; 
+        minY = 10.^xi(islocalmin(f));
+        minY = minY(1); 
+        %sometimes falsely gets upper thresshold
+        if minY > 2e3
+            minY = gl2_noise_thresh;
+        end
+    else %if no minimum, go just below trigger threshhold 
+        minY = gl2_noise_thresh;
+    end
+    end
+
+  if phase == 2
+          minY = prctile(fcsdat(in_syn,npar_synY),10)* .3; 
+          eukminX = prctile(fcsdat(in_euk, npar_eukX), 10)*.3; 
+  end
+
+
+    if phase ~=2 
+    %that worked pretty well let's do it for euks too
+    [f, xi] = ksdensity(log10(fcsdat(in_euk & log10(fcsdat(:, npar_eukX))< 3, npar_eukX)));
+    if sum(islocalmin(f))>0 & f(islocalmin(f))./sum(f) < .015; 
+        eukminX = 10.^xi(islocalmin(f));
+        eukminX = eukminX(1); 
+    else %if no minimum, go just below trigger threshhold 
+        eukminX = 175;
+        if phase >2
+        eukminX = 700; 
+        end
+  
+    end
+    end
+
+  
+    %make new gates with adapted boundaries
+    gsyn_main_gate(:,2) = [minY; minY; maxY; maxY]; 
+    gsyn_main_gate(1,1) = minX; 
+    gsyn_main_gate(4,1) = minX; 
+    geuk_main_gate(1,1) = eukminX; 
+    geuk_main_gate(5,1) = eukminX; 
+
+    if phase == 4; 
+        synGL1H2BL3Hslope = 1.33; %PE to CHL
+        synGL1H2BL3Hoffset = -.6; %PE to CHL 
+        
+        gsyn_main_gate(3,:) = [2000 60000]; 
+        gsyn_main_gate(4,:) = [80 1e6]; 
+            nonsynfactorB = 3; %2.5
+
+
+    end
+
+    % assingments for euk and syn 
+    in_euk = inpolygon(fcsdatlog(:,npar_eukX),fcsdatlog(:,npar_eukY),log10(geuk_main_gate(:,1)),log10(geuk_main_gate(:,2)));
+    in_syn = (inpolygon(fcsdatlog(:,npar_synX),fcsdatlog(:,npar_synY),log10(gsyn_main_gate(:,1)),log10(gsyn_main_gate(:,2))));
+
+
+    %% part 3
+
+    %look for things with low syn level phycoerythrin & low GL2/GL3 ratio?
+    %& not big FCS with low phycoerythrin
+    in_nonsyn_lowPE = fcsdat(:,npar_synY) > minY & fcsdat(:,npar_synY) < maxY/2 & fcsdat(:,npar_synY)./fcsdat(:,17) < nonsynfactorB & ~(fcsdat(:,npar_synY)<200);
+    in_nonsyn_hiPE = fcsdat(:,npar_synY) > maxY/2 & fcsdat(:,npar_synY)./fcsdat(:,17) > nonsynfactorB & fcsdat(:,npar_synY)./fcsdat(:,17) < nonsynfactorA;
+
+
+    %assign values in class vector
+    class(in_nonsyn_lowPE) = 3;
+    class(in_nonsyn_hiPE) = 4;
+
+    class(in_syn) = 2; %must be done after nonsyn
+
+    %now use diagonal line in plot 1 to distinguish syn from euks and coincident
+    class((fcsdatlog(:,npar_synY)<fcsdatlog(:,15)*synGL1H2BL3Hslope+synGL1H2BL3Hoffset & fcsdat(:,15)> min(geuk_main_gate(:,1))) & fcsdat(:, npar_synY)> minY) = 3; %more euks
+    class(in_syn & (fcsdatlog(:,npar_synY)<fcsdatlog(:,15)*synGL1H2BL3Hslope+synGL1H2BL3Hoffset & fcsdat(:,15)> min(geuk_main_gate(:,1)))) = 5; %Syn&Euk coincident
+    %also use FSC-W to see coincidence?
+
+     if phase == 4
+        class(~in_syn & fcsdat(:,npar_synY)> gsyn_main_gate(3,2)) = 4; 
+        class(class == 4 & fcsdatlog(:,npar_synY)>fcsdatlog(:,15)*synGL1H2BL3Hslope+synGL1H2BL3Hoffset) = 2; 
+     end
+
+    class(in_euk) = 1; %AFTER lowPE
+
+    %rule out eukaryotes classed with smaller ssc than syn minimum
+    class(fcsdat(:, npar_synX)<minX) = 0; 
+   
+    %use size to chl ratio to rule out noise between syn and euks
+    class(class ~= 2 & fcsdatlog(:,npar_eukX)<fcsdatlog(:,npar_synX)*syneukBL3H2SSCHslope+syneukBL3H2SSCHoffset) = 0; %more noise
+    
+    %find "syn, euk coincident" mean CHL
+    %look for "low PE euks" with less chlorophyll, and mark them noise
+    meancoincX = nanmean(fcsdat(class==6, npar_synX));
+    class(class == 6 & fcsdat(:,npar_synX)<meancoincX) = 0; 
+
+    %group things with very high PE signals. 
+    in_nonsyn_hiPE = fcsdat(:,npar_synY) > maxY;
+    class(in_nonsyn_hiPE) = 4;
+
+    class(fcsdat(:,npar_eukX) < 100 & fcsdat(:,npar_synY) < 2) = 0; %noise %LAST
+ 
+   
+    %save gate boundaries to pass to moviemaker 
+    bounds = {geuk_main_gate, gsyn_main_gate, synGL1H2BL3Hslope, synGL1H2BL3Hoffset, synGL1A2GL1Hmax, syneukBL3H2SSCHslope, syneukBL3H2SSCHoffset, nonsynfactorA, nonsynfactorB}; 
+ 
+
+end
+    
+
+    
