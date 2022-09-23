9c47237f89a749cc3d826876ae99238260e49dea
hsosik <hsosik@whoi.edu>
2022-09-23 12:13:45 -0400

    update webread format, include option for SPIROPA sections north of 39.5 latitude

--- IFCB_processing/plot_IFCB_sampling_year_vs_day_transect.m
+++ IFCB_processing/plot_IFCB_sampling_year_vs_day_transect.m
@@ -1,9 +1,32 @@
-myreadtable = @(filename)readtable(filename, 'Format', '%s%s%s %f%f%f%f%f %s%s %f%s%f %s%s%s%s%s %f'); %case with 5 tags
-metaT =  webread('https://ifcb-data.whoi.edu/api/export_metadata/NESLTER_transect', weboptions('Timeout', 60, 'ContentReader', myreadtable));
+%myreadtable = @(filename)readtable(filename, 'Format', '%s%s%s %f%f%f%f%f %s%s %f%s%f %s%s%s%s%s %f'); %case with 5 tags
+%metaT =  webread('https://ifcb-data.whoi.edu/api/export_metadata/NESLTER_transect', weboptions('Timeout', 60, 'ContentReader', myreadtable));
+opts = delimitedTextImportOptions("NumVariables", 21);
+opts.DataLines = [2 inf];
+%case with 5 tags
+opts.VariableTypes = ["string", "string", "string", "double", "double", "double", "double", "double", "string", "string", "double", "string", "double", "string", "string", "string", "string", "string", "string", "double", "double"];
+opts.VariableNamesLine = 1;
+myreadtable = @(filename)readtable(filename, opts);
+metaT = webread('https://ifcb-data.whoi.edu/api/export_metadata/NESLTER_transect', weboptions('Timeout', 60, 'ContentReader', myreadtable));
+  
 mdate = datenum(metaT.sample_time, 'yyyy-mm-dd HH:MM:ss+00:00');
 ind = strmatch('underway', metaT.sample_type);
-ind2 = strmatch(' ', char(metaT.sample_type));
-ind = [ind; ind2]; clear ind2
+%fix this later if want to include May 2017 cruise 
+%ind2 = strmatch(' ', char(metaT.sample_type));
+%ind = [ind; ind2]; clear ind2
+mdate = mdate(ind);
+
+%%
+%case to include SPIROPA north of 39.5 latitude
+opts = delimitedTextImportOptions("NumVariables", 20);
+opts.DataLines = [2 inf];
+opts.VariableTypes = ["string", "string", "string", "double", "double", "double", "double", "double", "string", "string", "double", "string", "double", "string", "string", "string", "string", "string", "double", "double"];
+opts.VariableNamesLine = 1;
+myreadtable = @(filename)readtable(filename, opts); %
+metaT =  webread('https://ifcb-data.whoi.edu/api/export_metadata/SPIROPA', weboptions('Timeout', 60, 'ContentReader', myreadtable));
+mdate2 = datenum(metaT.sample_time, 'yyyy-mm-dd HH:MM:ss+00:00');
+ind = (strcmp('underway', metaT.sample_type) & metaT.latitude>=39.5);
+mdate = [mdate; mdate2(ind)];
+%%
 
 %uw = load('c:\work\LTER\uw_all');
 %uw.uw_sum = [];
@@ -17,9 +40,9 @@ d = datevec(mdate);
 yd = mdate-datenum(d(:,1),1,0);
 
 figure
-plot(yd(ind), d(ind,1), 'r.', 'markersize',20)
+plot(yd, d(:,1), 'r.', 'markersize',20)
 datetick('x', 'm')
-ylim([min(d(ind,1))-.2 max(d(ind,1))+.2])
+ylim([min(d(:,1))-.2 max(d(:,1))+.2])
 set(gca, 'ydir', 'rev', 'ytick', min(d(:,1)):max(d(:,1)))
 set(gcf, 'position', [550 200 300 340])
 
