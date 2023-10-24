17a1fa9762a1c633b140586f351236777da0ba0c
hsosik <hsosik@whoi.edu>
2023-10-23 20:43:55 -0400

    plot distribution of year days by year for IFCB data collected on NES-LTER broadscale cruises

---
+++ IFCB_processing/plot_IFCB_sampling_year_vs_day_broadscale.m
@@ -0,0 +1,44 @@
+%myreadtable = @(filename)readtable(filename, 'Format', '%s%s%s %f%f%f%f%f %s%s %f%s%f %s%s%s%s%s %f'); %case with 5 tags
+%metaT =  webread('https://ifcb-data.whoi.edu/api/export_metadata/NESLTER_broadscale', weboptions('Timeout', 60, 'ContentReader', myreadtable));
+%metaT =  webread('https://ifcb-data.whoi.edu/api/export_metadata/NESLTER_broadscale', weboptions('Timeout', 60));
+opts = delimitedTextImportOptions("NumVariables", 24);
+opts.DataLines = [2 inf];
+%case with 8 tags
+opts.VariableTypes = ["string", "string", "string", "double", "double", "double", "double", "double", "string", "string", "double", "string", "double", "string", "string", "string", "string", "string", "string", "string", "string", "string", "double", "double"];
+opts.VariableNamesLine = 1;
+myreadtable = @(filename)readtable(filename, opts);
+metaT = webread('https://ifcb-data.whoi.edu/api/export_metadata/NESLTER_broadscale', weboptions('Timeout', 60, 'ContentReader', myreadtable));
+   
+mdate = datenum(metaT.sample_time, 'yyyy-mm-dd HH:MM:ss+00:00');
+%ind = strmatch('underway', metaT.sample_type);
+%ind2 = strmatch(' ', char(metaT.sample_type));
+%ind = [ind; ind2]; clear ind2
+
+%uw = load('c:\work\LTER\uw_all');
+%uw.uw_sum = [];
+%for ii = 1:length(uw.cruises)
+%    uw.uw_sum = [uw.uw_sum; datenum(uw.uw_all{ii}.date,'yyyy-mm-dd hh:MM:ss') uw.uw_all{ii}.(uw.vname_uw{ii}{3}) uw.uw_all{ii}.(uw.vname_uw{ii}{4})  uw.uw_all{ii}.(uw.vname_uw{ii}{1}) uw.uw_all{ii}.(uw.vname_uw{ii}{2})];
+%end
+%d = datevec(uw.uw_sum(:,1));
+%yd = uw.uw_sum(:,1) - datenum(d(:,1),1,0);
+
+ind = find(strcmp(metaT.sample_type, 'underway') | ismissing(metaT.sample_type));
+d = datevec(mdate(ind));
+yd = mdate(ind)-datenum(d(:,1),1,0);
+
+figure
+plot(yd, d(:,1), 'r.', 'markersize',20)
+xlim([1 365])
+datetick('x', 'm', 'keeplimits')
+datetick('x', 'm')
+ylim([min(d(:,1))-.2 max(d(:,1))+.2])
+set(gca, 'ydir', 'rev', 'ytick', min(d(:,1)):max(d(:,1)))
+set(gcf, 'position', [550 200 300 340])
+
+
+figure
+plot(d(:,1),yd, 'r.', 'markersize',10)
+ylim([1 365])
+datetick('y', 'm', 'keeplimits')
+xlim([min(d(:,1))-.2 max(d(:,1))+.2])
+set(gcf, 'position', [645 180 800 180])
