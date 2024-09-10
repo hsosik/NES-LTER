57febd029d7d3e18bec6c28e772107bb4a15b8aa
Emily Peacock <epeacock@whoi.edu>
2024-09-10 19:32:52 +0000

    EP commiting changes to plot_Attune_FCB_EPICS

--- Attune/Discrete_Samples/plot_Attune_FCB_EPICS.m
+++ Attune/Discrete_Samples/plot_Attune_FCB_EPICS.m
@@ -7,16 +7,20 @@ load('\\sosiknas1\Lab_data\MVCO\FCB\summary\FCB_compiledC_tables.mat')
 load('\\sosiknas1\Lab_data\Attune\cruise_data\MVCO\preserved\outputs\SummaryTable.mat')
 
 %load EPICS data?
-load('\\sosiknas1\Lab_data\MVCO\EPICS_MVCO_FCM_analysis\wksp_1May2014.mat')
+%load('\\sosiknas1\Lab_data\MVCO\EPICS_MVCO_FCM_analysis\wksp_1May2014.mat')
 %make EPICS workspace date usable
-dateplot = datetime(datevec(matdate), 'Format', 'uuuu-MM-dd HH:mm:ss');
-
+%dateplot = datetime(datevec(matdate), 'Format', 'uuuu-MM-dd HH:mm:ss');
+load('\\sosiknas1\Lab_data\MVCO\EPICS_MVCO_FCM_analysis\classify_FCMs.mat')
+tempEPICSdate = cell2mat(FCMs_classified(:,3));
+dateplot = datetime(datevec(tempEPICSdate), 'Format', 'uuuu-MM-dd HH:mm:ss');
+EPICSsynconc=cell2mat(FCMs_classified(:,6))./(cell2mat(FCMs_classified(:,9))*0.1);
+EPICSeukconc=cell2mat(FCMs_classified(:,7))./(cell2mat(FCMs_classified(:,9))*0.1);
 
 figure
-plot(TTsumnum.Time, TTsumnum.Syn./TTsumnum.ml, 'k.', 'MarkerSize', 3)
+plot(TTsumnum.Time, TTsumnum.Syn./TTsumnum.ml,'.', 'Color',[.5 .5 .5], 'MarkerSize', 2)
 hold on
-plot(CNTable.date_sampled, CNTable.syn_per_ml, 'r.', 'MarkerSize', 8)
-plot(dateplot, synconc, 'g.', 'MarkerSize', 8)
+plot(CNTable.date_sampled, CNTable.syn_per_ml, '.', 'Color',[1 0 0], 'MarkerSize', 9)
+plot(dateplot, EPICSsynconc,'.', 'Color', [.6 .2 .2], 'MarkerSize', 9)
 legend('FCB', 'Attune', 'EPICS2014');
 title('Syn');
 ylabel('Syn per ml')
@@ -25,11 +29,11 @@ grid
 datetick('keeplimits')
 
 figure
-plot(TTsumnum.Time, TTsumnum.Euk_total./TTsumnum.ml, 'k.', 'MarkerSize', 3)
+plot(TTsumnum.Time, TTsumnum.Euk_total./TTsumnum.ml, '.', 'Color',[.5 .5 .5], 'MarkerSize', 2)
 hold on
-plot(CNTable.date_sampled, CNTable.euk_per_ml, 'r.', 'MarkerSize', 8)
-%plot(dateplot, synconc, 'g.', 'MarkerSize', 8)
-legend('FCB', 'Attune');
+plot(CNTable.date_sampled, CNTable.euk_per_ml, '.', 'Color',[0 1 0], 'MarkerSize', 9)
+plot(dateplot, EPICSeukconc, '.', 'Color',[.2 .6 .4], 'MarkerSize', 9)
+legend('FCB', 'Attune', 'EPICS2014');
 title('Euks');
 ylabel('Euks per ml')
 set(gcf, 'Position', [87 100 1200 400])
