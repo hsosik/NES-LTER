696c2c6ab5a034551126f6c75bfb68fb0fe9aadf
hsosik <hsosik@whoi.edu>
2025-05-14 16:50:17 -0400

    work around to get ml_analyzed while OOI Pioneer Mooring has no dashboard

---
+++ IFCB_processing/ooi_mooring_metadata_hack.m
@@ -0,0 +1,13 @@
+t = dir('\\sosiknas1\IFCB_data\ooi\pioneer_mooring\data\\**\*.roi');
+p = {t.name}';
+meta_data = table;
+meta_data.pid = regexprep({t.name}', '.roi', '');
+meta_data.sample_time = datetime(IFCB_file2date(meta_data.pid), 'ConvertFrom', 'datenum', 'Format', 'uuuu-MM-dd'' ''HH:mm:ss+00:00');
+meta_data.ml_analyzed = IFCB_volume_analyzed(strcat({t.folder}', filesep, regexprep({t.name}', 'roi', 'hdr')));
+meta_data.sample_type
+
+bd = dir('\\sosiknas1\IFCB_data\ooi\pioneer_mooring\data\beads\*.roi');
+[a,b,c] = intersect(regexprep({b.name}', '.roi', ''),meta_data.pid);
+meta_data.sample_type(:) = {'mooring'};
+meta_data.sample_type(c) = {'bead'};
+meta_data.skip(:) = 0;
